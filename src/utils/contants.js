830ed1adf23027d1d62f251ac6fe8cfdce6d20f4
rifazahmed7 <158307114+rifazahmed7@users.noreply.github.com>
2024-08-09 20:58:23 +0530

    Update contants.js

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyCoCJ1kA4O10r1m_lNXLy6WAz8Mp2XEX-M";
+const GOOGLE_API_KEY = "AIzaSyB3bZLl10Cc9lCk39MIR7NCdkMqVfAY3v0";
 
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
